4bf11972dc283a6ec3e857c3eafe25031be89317
Saull Brandão <saull@outlook.com>
2023-01-06 10:04:51 -0300

    display courses list

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
 })
-export class CursosListaComponent {
+export class CursosListaComponent implements OnInit {
+  cursos$: Observable<Curso[]> = new Observable();
 
+  constructor(private cursosService: CursosService) {}
+
+  ngOnInit(): void {
+    this.cursos$ = this.cursosService.list();
+  }
 }
